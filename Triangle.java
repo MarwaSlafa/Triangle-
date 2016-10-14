949cd45655bd98397f82a9482231e2dc7e51140b
Marwa <marwa.slafa@gmail.com>
2016-10-14 13:14:32 +0200

    ajout le code de Triangle

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+public class Triangle {
+
+	public static void main(String[] args) {
+		String etoile=" ";
+		for (int i=0;i<8;i++){
+			int espace=8-i;
+			for (int j=0;j<espace;j++){
+				System.out.print(" ");
+			}
+			etoile=etoile+"* ";
+			System.out.println(etoile);
+		}
+
+	}
+ }
