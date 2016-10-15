3cac0709feb413e35c417f238bf606eb693d712c
Asmaa Bengueddach <asmaa.bengueddach@gmail.com>
2016-10-15 23:39:40 +0200

    Unpdate Triangle.java
     public static void main(String[] args)
        {
            int space, ligne;
            String etoile = "";
    
            Scanner read = new Scanner(System.in);
            
            System.out.println("Donner le nombre de ligne du triangle ");
            ligne = read.nextInt();
    
            for (int i = 0; i < ligne; i++) {
    
                space = (ligne-1) - i;
    
                for (int j = 0; j < space; j++)
                {
    
                    System.out.print(" ");
    
                }
                etoile = etoile + "* ";
                System.out.println(etoile);
    
            }
        }

--- Triangle.java
+++ Triangle.java
@@ -2,7 +2,7 @@ public class Triangle {
 
 	public static void main(String[] args) {
 		String etoile=" ";
-		for (int i=0;i<8;i++){
+		for (int i=0;i<8;i++){/*faire varier les lignes */
 			int espace=8-i;
 			for (int j=0;j<espace;j++){
 				System.out.print(" ");
